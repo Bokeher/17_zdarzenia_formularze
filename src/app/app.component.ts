82c517a8c47c1f3a874a5b1ed788581b5b1bca09
Bokeher <bokeher47@gmail.com>
2023-06-04 18:16:18 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '17_zdarzenia_formularze_rychter_szymon';
+}
